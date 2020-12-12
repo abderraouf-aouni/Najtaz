5485fdd0f41b6edba7588d02a1e74f27c11f47af
abderraouf-aouni <loufa.aouni@gmail>
2020-12-12 20:21:34 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,98 @@
+let selectBox = document.querySelector('header .user select');
+selectBox.selectedIndex = -1;
+let days = document.querySelectorAll('.days .week .day ');
+let header = document.querySelector('header');
+
+days.forEach(day => day.addEventListener('click', () => {
+    let dayName = day.children[1];
+    let radioButton = day.children[0];
+    removeCurrentClass()
+    dayName.classList.add('current')
+    radioButton.checked = true
+}))
+document.addEventListener("click", closeAllSelect);
+window.addEventListener('scroll', scrolled)
+
+function scrolled() {
+    if (window.scrollY > 0) {
+        header.classList.add('moved')
+    } else {
+        header.classList.remove('moved')
+    }
+}
+function removeCurrentClass() {
+    days.forEach(day => {
+        let dayName = day.children[1]
+        dayName.classList.remove('current')
+    })
+}
+
+
+
+/* Customize Select Box */
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    yl = y.length;
+                    for (k = 0; k < yl; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+    });
+}
+
+function closeAllSelect(elmnt) {
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
+    }
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
+    }
+}
+
